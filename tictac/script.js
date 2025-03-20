de0edcfc21367c65d8b0ebd7cb2916592f541bff
Rachit <rachitdua997@gmail.com>
2025-03-21 00:17:27 +0530

    Update script.js

--- tictac/script.js
+++ tictac/script.js
@@ -50,7 +50,6 @@ Array.from(boxes).forEach(element =>{
     })
 })
 
-// Add onclick listener to reset button
 reset.addEventListener('click', ()=>{
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
